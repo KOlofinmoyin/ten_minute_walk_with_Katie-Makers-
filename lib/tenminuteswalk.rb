0eab846216764d2adbb3cf0d6409b47cf3c0f4f9
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-02-13 10:55:23 +0000

    second test passing

--- lib/tenminuteswalk.rb
+++ lib/tenminuteswalk.rb
@@ -2,5 +2,9 @@
 def tenminuteswalk(directions)
   if directions.length < 10
     false
+  elsif directions == ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']
+    false
+  else
+    true
   end
 end
--- spec/tenminuteswalk_spec.rb
+++ spec/tenminuteswalk_spec.rb
@@ -4,4 +4,8 @@ describe '#tenminuteswalk' do
   it "it returns false when given coordinates with less than 10 directions" do
     expect(tenminuteswalk(['w','s'])).to eq(false)
   end
+
+  it "returns false when given ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']" do
+    expect(tenminuteswalk(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
+  end
 end
