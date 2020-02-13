76a1cdf4f0a78d019742342ccc4978a770ab06d4
Katie Rodgers <katierodgers95@gmail.com>
2020-02-13 11:50:18 +0000

    refactored methods checking even number of coordinates

--- lib/tenminuteswalk.rb
+++ lib/tenminuteswalk.rb
@@ -2,27 +2,31 @@
 def tenminuteswalk(directions)
   north = 0
   south = 0
+  east = 0
+  west = 0
   
-  if directions.length < 10
+  if directions.length != 10
     false
-  elsif directions == ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']
-    false
-  elsif directions.length == 10
+    elsif 
     directions.each do |coordinate| 
     if coordinate == 's'
         south += 1
       elsif coordinate == 'n'
         north += 1
+      elsif coordinate == 'w'
+        west += 1
+      elsif coordinate == 'e'
+        east += 1
       end
     end
 
-  if south != north
-    false
-   else
-    true
-  end
-
-  else
-    true
+    if south != north
+      false
+      elsif east != west
+        false
+      else
+        true
+    end
   end
 end
+    
