b5f5b662493275ece63a4dfc1e15bb7bcd5ccc7d
Katie Rodgers <katierodgers95@gmail.com>
2020-02-13 11:53:44 +0000

    ten minute walk app completed

--- spec/tenminuteswalk_spec.rb
+++ spec/tenminuteswalk_spec.rb
@@ -12,5 +12,8 @@ describe '#tenminuteswalk' do
   it "returns false when number of north and south coordinates are uneven " do
     expect(tenminuteswalk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])).to eq(false)
   end
-  
+
+  it "returns true when directions equal to ten minutes and return to starting point ie ['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w']" do
+    expect(tenminuteswalk(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
+  end
 end
