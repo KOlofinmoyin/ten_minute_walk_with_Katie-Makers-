59b8d44929a32d45f073f2e21aac80a8dee6a4ab
Katie Rodgers <katierodgers95@gmail.com>
2020-02-13 11:41:36 +0000

    checking south and north values to be equal

--- lib/tenminuteswalk.rb
+++ lib/tenminuteswalk.rb
@@ -1,9 +1,27 @@
 
 def tenminuteswalk(directions)
+  north = 0
+  south = 0
+  
   if directions.length < 10
     false
   elsif directions == ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']
     false
+  elsif directions.length == 10
+    directions.each do |coordinate| 
+    if coordinate == 's'
+        south += 1
+      elsif coordinate == 'n'
+        north += 1
+      end
+    end
+
+  if south != north
+    false
+   else
+    true
+  end
+
   else
     true
   end
--- spec/tenminuteswalk_spec.rb
+++ spec/tenminuteswalk_spec.rb
@@ -8,4 +8,9 @@ describe '#tenminuteswalk' do
   it "returns false when given ['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w']" do
     expect(tenminuteswalk(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
   end
+
+  it "returns false when number of north and south coordinates are uneven " do
+    expect(tenminuteswalk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])).to eq(false)
+  end
+  
 end
